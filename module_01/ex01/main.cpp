ba438ac39dbf096e4d7ca97b9a2085146b9de77e
Chavert home <chavert@live.nl>
2023-11-01 15:21:18 +0100

    ex00 finished

---
+++ module_01/ex01/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: chaverttermaat <chaverttermaat@student.      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/01 15:12:56 by chavertterm   #+#    #+#                 */
+/*   Updated: 2023/11/01 15:14:08 by chavertterm   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+int	main(void)
+{
+	int horde_size = 10;
+	Zombie *zombie;
+
+	
+}
