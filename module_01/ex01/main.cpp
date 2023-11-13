55fbe905775723dcfe03298cb07a675334830e51
Chavert home <chavert@live.nl>
2023-11-13 10:06:08 +0100

    added ex00 and ex01 to module_01

--- module_01/ex01/main.cpp
+++ module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chaverttermaat <chaverttermaat@student.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/01 15:12:56 by chavertterm   #+#    #+#                 */
-/*   Updated: 2023/11/01 15:14:08 by chavertterm   ########   odam.nl         */
+/*   Updated: 2023/11/02 11:36:33 by chavertterm   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,14 @@
 int	main(void)
 {
 	int horde_size = 10;
-	Zombie *zombie;
+	Zombie *zombie_array;
 
-	
+	zombie_array = zombieHorde(horde_size, "zombie");
+	for (int i = 0; i < horde_size; i++){
+		std::cout << "nr: " << i << " ";
+		zombie_array[i].announce();
+	}
+	delete[] zombie_array;
+	system("leaks zombieHorde");
+	return(0);
 }
