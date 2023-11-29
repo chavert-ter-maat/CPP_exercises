9c4e0f390cb31ef2566e1650cebdd3cd2811367d
Chavert Ter Maat <cter-maa@f1r5s11.codam.nl>
2023-11-29 16:45:31 +0100

    bla

--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cter-maa <cter-maa@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/22 09:27:43 by chavertterm   #+#    #+#                 */
-/*   Updated: 2023/11/28 10:29:25 by chavertterm   ########   odam.nl         */
+/*   Updated: 2023/11/29 16:45:07 by cter-maa      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,21 @@ int	main(void)
 		std::cout << form1 << std::endl;
 		form1.beSigned(dude1);
 		std::cout << form1 << std::endl;
-	// std::cout << form1 << std::endl;
 	}
-	catch (const char *error)
-	{
+	catch (const char *error){
 		std::cout << error << std::endl;
 	}
-	// try{
-	// 	Bureaucrat dude1("dude1", 15);
-	// 	Form form1("form1");
-	// 	std::cout << form1 << std::endl;
-	// 	dude1.signForm(form1);
-	// 	std::cout << form1 << std::endl;
-	// }
-	// catch (const char *error)
-	// {
-	// 	std::cerr << error << std::endl;
-	// }
+	
+	std::cout << "test for signForm" << std::endl;
+	try{
+		Bureaucrat dude1("dude1", 15);
+		Form form1("form1");
+		std::cout << form1 << std::endl;
+		dude1.signForm(form1);
+		std::cout << form1 << std::endl;
+	}
+	catch (const char *error){
+		std::cerr << error << std::endl;
+	}
 	return 0;
 }
